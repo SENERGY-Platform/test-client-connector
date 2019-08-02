2734fa747c51492dedafd7aed3ef5b83c947bf5f
y-du <42994541+y-du@users.noreply.github.com>
2019-08-02 13:08:45 +0200

    reimplementation

--- client.py
+++ client.py
@@ -1,52 +1,85 @@
-import cc_lib, logging, random, json, datetime, time
+"""
+   Copyright 2019 InfAI (CC SES)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
 
 
-logger = cc_lib.logger.getLogger('test-client')
-logger.setLevel(logging.INFO)
+from test.configuration import config
+from test.logger import root_logger
+from test.device_manager import DeviceManager
+from test.sensor.types import TestSensor
+from test.sensor.handler import SensorHandler
+from test.actuator.types import TestActuator
+from test.actuator.handler import ActuatorHandler
+import cc_lib, time
 
 
-class PushReading(cc_lib.types.SensorService):
-    uri = "iot#995f438d-7425-497b-8c23-6a29394a6a52"
-    description = "Push current meter reading"
-    name = "Push counter"
+logger = root_logger.getChild(__name__)
 
 
-class FerrarisOpticalSensorType(cc_lib.types.Device):
-    uri = "iot#fd0e1327-d713-41da-adfb-e3853a71db3b"
-    description = "Test Ferraris Sensor"
-    services = {
-        "detection": PushReading
-    }
-
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
-
-ferraris_sensor = FerrarisOpticalSensorType("0058-2345", "Ferraris Sensor")
+device_manager = DeviceManager()
 
 
 def on_connect(client: cc_lib.client.Client):
+    devices = device_manager.devices
+    for device in devices:
+        try:
+            if device.enabled:
+                client.connectDevice(device, asynchronous=True)
+        except cc_lib.client.DeviceConnectError:
+            pass
+
+
+connector_client = cc_lib.client.Client()
+connector_client.setConnectClbk(on_connect)
+
+sensor_device = TestSensor(config.Sensor.id, config.Sensor.name, config.Sensor.enable)
+device_manager.add(sensor_device)
+actuator_device = TestActuator(config.Actuator.id, config.Actuator.name, config.Actuator.enable)
+device_manager.add(actuator_device)
+
+sensor_handler = SensorHandler(sensor_device, connector_client)
+actuator_handler = ActuatorHandler(actuator_device, connector_client)
+
+if __name__ == '__main__':
     try:
-        client.connectDevice(ferraris_sensor, asynchronous=True)
-    except cc_lib.client._exception.DeviceConnectError:
-        pass
-
-
-test_client = cc_lib.client.Client()
-test_client.setConnectClbk(on_connect)
-test_client.connect(reconnect=True)
-
-time.sleep(5)
-
-reading = random.random() * 100
-
-while True:
-    reading += random.random()
-    msg = cc_lib.client.message.Message(
-        json.dumps({"time": '{}Z'.format(datetime.datetime.utcnow().isoformat()), "unit": "kWh", "value": reading})
-    )
-    event = cc_lib.client.message.Envelope(ferraris_sensor, "detection", msg)
-    logger.info("Sending:\n{}".format(event))
-    test_client.emmitEvent(event, asynchronous=True)
-    time.sleep(10)
+        while True:
+            try:
+                connector_client.initHub()
+                break
+            except cc_lib.client.HubInitializationError:
+                time.sleep(5)
+        while True:
+            try:
+                for device in device_manager.devices:
+                    connector_client.addDevice(device)
+                break
+            except cc_lib.client.DeviceAddError:
+                time.sleep(5)
+        while True:
+            try:
+                connector_client.syncHub(device_manager.devices)
+                break
+            except cc_lib.client.HubSyncError:
+                time.sleep(5)
+        connector_client.connect(reconnect=True)
+        if config.Actuator.enable:
+            actuator_handler.start()
+        if config.Sensor.enable:
+            sensor_handler.start()
+        for handler in (actuator_handler, sensor_handler):
+            if handler.is_alive():
+                handler.join()
+    except KeyboardInterrupt:
+        print("\ninterrupted by user\n")
