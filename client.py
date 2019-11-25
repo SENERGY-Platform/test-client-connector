c078fc246cb7f391f6fd0899093db9002f2b77b9
y-du <42994541+y-du@users.noreply.github.com>
2019-11-25 11:00:21 +0100

    generate device IDs if none are given

--- client.py
+++ client.py
@@ -22,7 +22,7 @@ from test.sensor.types import TestSensor
 from test.sensor.handler import SensorHandler
 from test.actuator.types import TestActuator
 from test.actuator.handler import ActuatorHandler
-import cc_lib, time
+import cc_lib, time, uuid
 
 
 logger = root_logger.getChild(__name__)
@@ -44,8 +44,15 @@ def on_connect(client: cc_lib.client.Client):
 connector_client = cc_lib.client.Client()
 connector_client.setConnectClbk(on_connect)
 
+if not config.Sensor.id:
+    config.Sensor.id = uuid.uuid4()
+
 sensor_device = TestSensor(str(config.Sensor.id), str(config.Sensor.name), config.Sensor.enable)
 device_manager.add(sensor_device)
+
+if not config.Actuator.id:
+    config.Actuator.id = uuid.uuid4()
+
 actuator_device = TestActuator(str(config.Actuator.id), str(config.Actuator.name), config.Actuator.enable)
 device_manager.add(actuator_device)
 
